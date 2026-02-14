fb8051d5f612a4a3328dc2d005d28d644dd76d59
Lianelba <Lianelbaovallegil@gmail.com>
2026-02-14 13:08:42 -0400

    commit

---
+++ workspace/Porton/main/porton.c
@@ -0,0 +1,200 @@
+//Nombre: Lianelba Ovalle Gil
+//Matricula: 2023-1758
+//Materia: Microcontroladores
+//Profesor: Carlos Pichardo
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "freertos/timers.h"
+
+#define Estado_Inicio 0
+#define Estado_Abierto 1
+#define Estado_Cerrado 2
+#define Estado_Abriendo 3
+#define Estado_Cerrando 4
+#define Estado_Desconocido 5
+#define Estado_Error 6
+#define Estado_Stop 7
+
+#define Motor_ON 1
+#define Motor_OFF 0
+#define Lamp_ON 1
+#define Lamp_OFF 0
+#define Buzzer_ON 1
+#define Buzzer_OFF 0
+
+struct IO {
+    unsigned int fca; //final carrera abierto
+    unsigned int fcc; //final carrera cerrado
+    unsigned int ftc; //fotoresistor
+    unsigned int bc;  //boton cerrar
+    unsigned int ba;  //boton abrir
+    unsigned int bs;  //boton stop
+    unsigned int be;      //boton error
+    //unsigned int pp; no se que hace
+    unsigned int mc;      //motor cerrando
+    unsigned int ma;      //motot abriendo
+    unsigned int lamp;    //lampara
+    unsigned int buzzer;  //buzzer
+} io;
+
+int Estado_Actual = Estado_Inicio;
+int Estado_Siguiente = Estado_Inicio;
+int Estado_Anterior = Estado_Stop;
+
+TimerHandle_t xTimers;
+int interval = 100;
+int timerId = 1;
+
+
+
+esp_err_t set_timer(void);
+
+void vTimerCallback(TimerHandle_t pxTimer)
+{
+	ESP_LOGI(tag, "Event was called from timer");
+}
+
+int Func_Estado_Inicio(void);
+int Func_Estado_Abierto(void);
+int Func_Estado_Cerrado(void);
+int Func_Estado_Abriendo(void);
+int Func_Estado_Cerrando(void);
+int Func_Estado_Stop(void);
+int Func_Estado_Error(void);
+
+esp_err_t set_timer (void)
+{
+	ESP_LOGI(tag, "Timer init configuration");
+	xTimers = xTimerCreate ( "Timer",
+								(pdMS_TO_TICKS(interval)),
+								pdTRUE,
+								(void *)timerId,
+								vTimerCallback
+							  );
+	if (xTimers == NULL)
+	{
+		ESP_LOGE(tag, "Timer nor created.");
+	}
+	else
+	{
+		if (xTimerStart(xTimers, 0) != pdPASS)
+		{
+			ESP_LOGI(tag, "Timer could not be set into the Active state");
+		}
+	}
+	return ESP_OK;
+}
+
+int Func_Estado_Inicio(void) {
+
+    io.mc = Motor_OFF;
+    io.ma = Motor_OFF;
+    io.lamp = Lamp_OFF;
+    io.buzzer = Buzzer_OFF;
+
+    if (io.fca)
+        Estado_Siguiente = Estado_Abierto;
+    else if (io.fcc)
+        Estado_Siguiente = Estado_Cerrado;
+    else
+        Estado_Siguiente = Estado_Stop;
+
+    return 0;
+}
+
+int Func_Estado_Abierto(void) {
+
+    io.mc = Motor_OFF;
+    io.ma = Motor_OFF;
+    io.lamp = Lamp_OFF;
+
+    if (io.bc) 
+        Estado_Siguiente = Estado_Cerrando;
+    }
+
+    return 0;
+}
+
+int Func_Estado_Cerrado(void) {
+
+    io.mc = Motor_ON;
+    io.ma = Motor_OFF;
+    io.lamp = Lamp_OFF;
+
+    if (io.ba) {
+        Estado_Siguiente = Estado_Abriendo;
+    }
+
+    return 0;
+}
+
+int Func_Estado_Abriendo(void) {
+
+    io.mc = Motor_OFF;
+    io.ma = Motor_ON;
+    io.lamp = Lamp_ON;
+
+    if (io.fca)
+        Estado_Siguiente = Estado_Abierto;
+    else if (io.bs){
+		Estado_Siguiente = 
+	}
+
+    return 0;
+}
+
+int Func_Estado_Cerrando(void) {
+
+    io.mc = Motor_ON;
+    io.ma = Motor_OFF;
+    io.lamp = Lamp_ON;
+
+    if (io.ba) {
+        Estado_Siguiente = Estado_Abriendo;
+    }
+    else if (io.bs){
+		Estado_Siguiente = Estado_Stop;
+	}
+	else if (io.fcc){
+		Estado_Siguiente = Estado_Cerrado;
+	}
+
+    return 0;
+}
+
+int Func_Estado_Stop(void) {
+
+    io.mc = Motor_OFF;
+    io.ma = Motor_OFF;
+    io.lamp = Lamp_ON;
+
+    if (io.ba) {
+        motor_start_tick = xTaskGetTickCount();
+        Estado_Siguiente = Estado_Abriendo;
+    }
+    else if (io.bc) {
+        motor_start_tick = xTaskGetTickCount();
+        Estado_Siguiente = Estado_Cerrando;
+    }
+
+    return 0;
+}
+
+int Func_Estado_Error(void) {
+
+    io.mc = Motor_OFF;
+    io.ma = Motor_OFF;
+    io.lamp = Lamp_ON;
+    io.buzzer = Buzzer_ON;
+
+    return 0;
+}
+
+
+
